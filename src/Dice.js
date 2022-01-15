75a8e5a9c5bc8bc0474eed2cb2221b7c4305af03
sbs1253 <rokaf340@gmail.com>
2022-01-15 15:18:26 +0900

    study

--- src/Dice.js
+++ src/Dice.js
@@ -1,7 +1,7 @@
 import diceBlue01 from './assets/dice-blue-1.svg';
 
 function Dice() {
-  return <img src='./assets/dice-blue-1.svg' alt="주사위" />;
+  return <img src={diceBlue01} alt="주사위" />;
 }
 
 export default Dice;
